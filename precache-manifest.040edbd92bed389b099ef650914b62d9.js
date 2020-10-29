36d20209ea329549c42876efb4f203513a3ab821
dmeats <djmmm@hotmail.com>
2020-10-29 13:30:04 -0400

    Updates

---
+++ precache-manifest.040edbd92bed389b099ef650914b62d9.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82b31d5141a318915ccbdbbd66ae0b1d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7bf8fd0bf377d7c760a2",
+    "url": "/portfolio/static/css/main.e9a43d34.chunk.css"
+  },
+  {
+    "revision": "156b5a21cb1b8c071584",
+    "url": "/portfolio/static/js/2.1508850a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/portfolio/static/js/2.1508850a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bf8fd0bf377d7c760a2",
+    "url": "/portfolio/static/js/main.2a81be07.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "f89ec8449c35418665c5daff03c51479",
+    "url": "/portfolio/static/media/Air_quality.f89ec844.png"
+  },
+  {
+    "revision": "cc9a2c3ed20bfaf65f55e021858f34af",
+    "url": "/portfolio/static/media/IOT_manuf.cc9a2c3e.png"
+  },
+  {
+    "revision": "8e6fd970fcae9c94fcc6b03da43cd87b",
+    "url": "/portfolio/static/media/ProcessMap.8e6fd970.png"
+  },
+  {
+    "revision": "48d800eacf624d4f2768e1902addfbee",
+    "url": "/portfolio/static/media/VegApp.48d800ea.png"
+  },
+  {
+    "revision": "c9a603766c8424cc95d10b5af2c09684",
+    "url": "/portfolio/static/media/business.c9a60376.png"
+  },
+  {
+    "revision": "ffac9ef573eb7114244cedcdf9d36f5c",
+    "url": "/portfolio/static/media/codescreen.ffac9ef5.png"
+  },
+  {
+    "revision": "5f6ef6210bccd3573eefae1013174f99",
+    "url": "/portfolio/static/media/dbmodel.5f6ef621.png"
+  },
+  {
+    "revision": "25a99d1b713347b93f25f09194754b6b",
+    "url": "/portfolio/static/media/digital_dash.25a99d1b.png"
+  },
+  {
+    "revision": "164e7b62b5df5b6ea911336d8691c3d8",
+    "url": "/portfolio/static/media/drum_machine.164e7b62.png"
+  },
+  {
+    "revision": "f8ad0e040bd485dc21f3811b8e87cebb",
+    "url": "/portfolio/static/media/groceryapp.f8ad0e04.png"
+  },
+  {
+    "revision": "fe47d6a4d3b961dd2e1f69584ecdf897",
+    "url": "/portfolio/static/media/large_Gear.fe47d6a4.png"
+  },
+  {
+    "revision": "77822c86dceb6beaf136f618b36bddd1",
+    "url": "/portfolio/static/media/mobileplanet.77822c86.png"
+  },
+  {
+    "revision": "79e3223bf1b6958102fec99c2e14a1f9",
+    "url": "/portfolio/static/media/paintboard.79e3223b.png"
+  },
+  {
+    "revision": "08f652c59889e88622fca71d9be2d596",
+    "url": "/portfolio/static/media/paintdetailimg.08f652c5.png"
+  },
+  {
+    "revision": "29a9d2245ed5dbb459775693e17f69a0",
+    "url": "/portfolio/static/media/planet.29a9d224.png"
+  },
+  {
+    "revision": "32c173bb84adc35645aa6afe671847d3",
+    "url": "/portfolio/static/media/power_app.32c173bb.png"
+  },
+  {
+    "revision": "6336825e4cae989a4edef26aed699dbe",
+    "url": "/portfolio/static/media/programming.6336825e.png"
+  },
+  {
+    "revision": "dfc9d935d9ed0d98a3b52214bf43d751",
+    "url": "/portfolio/static/media/zone_con.dfc9d935.png"
+  }
+]);
